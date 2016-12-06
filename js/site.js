651baf6cfb2a41547c425582902c28f44f20b726
AmyDaYe <dlee57@hawk.iit.edu>
2016-12-06 02:30:22 -0600

    change the not deleted line

--- js/site.js
+++ js/site.js
@@ -1,7 +1,6 @@
     
 $(document).ready(function() {
   //google.maps.event.addDomListener(window, 'load', initMap);
-    event.preventDefault();
 	function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: -34.397, lng: 150.644},
