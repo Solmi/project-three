2e142e6ef5c1a2acdef1993feb666b2896757bfc
AmyDaYe <dlee57@hawk.iit.edu>
2016-12-06 03:27:24 -0600

    delete the jquery source

--- js/site.js
+++ js/site.js
@@ -1,10 +1,10 @@
-    
-$(document).ready(function() {
+//$(document).ready(function() {
   //google.maps.event.addDomListener(window, 'load', initMap);
-	function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom:8
-    });
-   }
-});
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+//});
