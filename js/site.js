7f156b6f0a031b736be6e7165dd8a137d3f5bc80
AmyDaYe <dlee57@hawk.iit.edu>
2016-12-07 22:39:56 -0600

    add javascript file to show the map reway along the input result

--- js/site.js
+++ js/site.js
@@ -1,10 +1,41 @@
-//$(document).ready(function() {
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+$(document).ready(function() {
+  $('#gh-form').on('submit', function(event) {
+  
+  var state = document.getElementById('gh-location').value;
+
+  var geocoder = new google.maps.Geocoder();
+
+    event.preventDefault();
+
+    geocoder.geocode({
+      'address': state
+    }, function(results, status) {
+      initMap(results);
+    });
+
   });
-}
-  //google.maps.event.addDomListener(window, 'load', initMap);
-//});
+
+
+  function clearMap() {
+    document.getElementById('gh-form').reset();
+  }
+  
+  function initMap(state) {
+
+      // show the map
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 5,
+      center: state[0].geometry.location
+    });
+
+    var marker = new google.maps.Marker({
+      position: state[0].geometry.location,
+      map: map
+    });
+
+    //clear clear
+    clearMap();
+  }
+  
+});
